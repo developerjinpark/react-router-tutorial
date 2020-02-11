40c51263f06c76834e0baec33ae8cb5139c48c3b
Jin <jinpark1504@gmail.com>
2020-02-10 20:52:14 -0500

    finished tutorial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-import { Home, About } from 'pages';
+import { Home, About, Posts } from 'pages';
 import Menu from 'components/Menu';
 
 const App = () => {
@@ -11,6 +11,7 @@ const App = () => {
       <Route exact path="/" component={Home} />
       {/* <Route exact path="/about" component={About} /> */}
       <Route path="/about/:name?/:age?" component={About} />
+      <Route path="/posts" component={Posts} />
     </div>
   );
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
-import { NavLink } from 'react-router-dom';
+import { NavLink, withRouter } from 'react-router-dom';
 
 const Menu = () => {
     const activeStyle = {
@@ -14,6 +14,7 @@ const Menu = () => {
                 <li><NavLink exact to="/" activeStyle={activeStyle}>Home</NavLink></li>
                 <li><NavLink exact to="/about" activeStyle={activeStyle}>About</NavLink></li>
                 <li><NavLink to="/about/react" activeStyle={activeStyle}>Introduce React</NavLink></li>
+                <li><NavLink to="/posts" activeStyle={activeStyle}>List of Posts</NavLink></li>
             </ul>
         </div>
     )
